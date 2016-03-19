d61e954b6d2784bb9a8426224d99663cf0cbf523
Oliver Weichhold <oliver@weichhold.com>
2016-03-19 11:01:03 +0100

    Refactor

---
+++ AnimatedBackgroundWPF/AnimatedBackground.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace AnimatedBackgroundWPF
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class AnimatedBackground : UserControl
+    {
+        public AnimatedBackground()
+        {
+            InitializeComponent();
+        }
+
+        public Storyboard MainStoryboard => (Storyboard)this.Resources["MainStoryboard"];
+    }
+}
