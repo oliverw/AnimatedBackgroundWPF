e7e8891a36d54896861fa47b0a387c2573c71ff2
Oliver Weichhold <oliver@weichhold.com>
2016-03-19 10:55:05 +0100

    Initial

---
+++ AnimatedBackgroundWPF/UserControl1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public Storyboard MainStoryboard
+        {
+            get { return (Storyboard)this.Resources["MainStoryboard"]; }
+        }
+    }
+}
