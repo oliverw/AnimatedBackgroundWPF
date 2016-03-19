7d8728cf2a9e448f2aab830a1088c283da4a8162
Oliver Weichhold <oliver@weichhold.com>
2016-03-19 11:20:18 +0100

    Animated Opacity instead of Mask

---
+++ AnimatedBackgroundWPF/AnimatedBackgroundWithoutOpacityMask.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace AnimatedBackgroundWPF
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class AnimatedBackgroundWithoutOpacityMask : UserControl
+    {
+        public AnimatedBackgroundWithoutOpacityMask()
+        {
+            InitializeComponent();
+        }
+
+        public Storyboard MainStoryboard => (Storyboard)this.Resources["MainStoryboard"];
+    }
+}
